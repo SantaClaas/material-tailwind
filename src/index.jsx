4eb1d39189331f9d169968313739c78cef971526
Claas <claas.moehlmann@outlook.com>
2024-01-13 16:38:31 +0100

    Configure Tailwind CSS

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,7 @@
 /* @refresh reload */
-import { render } from 'solid-js/web'
+import { render } from "solid-js/web";
 
-import './index.css'
-import App from './App'
+import "./index.css";
+import App from "./App";
 
-const root = document.getElementById('root')
-
-render(() => <App />, root)
+render(() => <App />, document.body);
