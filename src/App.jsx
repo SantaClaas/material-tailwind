2834da299dcf07be3c95673841197e8ee48dd339
Claas <claas.moehlmann@outlook.com>
2024-01-13 17:52:51 +0100

    Test out some things

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,46 @@
+import {
+  argbFromHex,
+  themeFromSourceColor,
+  applyTheme,
+} from "@material/material-color-utilities";
+import { createMemo, createSignal } from "solid-js";
+// import configuration from "../tailwind.config";
+import defaultConfiguration from "tailwindcss/defaultConfig";
+
 function App() {
-  return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
+  const [color, setColor] = createSignal("#C14450");
+
+  const theme = () => {
+    return themeFromSourceColor(color());
+  };
+
+  const themeJson = () => {
+    return JSON.stringify(theme(), null, 2);
+  };
+
+  const defaultConfigurations = createMemo(() =>
+    JSON.stringify(defaultConfiguration, null, 2)
+  );
+
+  function handleColorChange(event) {
+    const color = argbFromHex(event.target.value);
+    setColor(event.target.value);
+  }
+
+  return (
+    <>
+      <label for="color">Select color</label>
+      <input
+        type="color"
+        id="color"
+        value={color()}
+        onInput={handleColorChange}
+      />
+      {/* <pre>{defaultConfigurations()}</pre> */}
+
+      <pre>{themeJson()}</pre>
+    </>
+  );
 }
 
 export default App;
